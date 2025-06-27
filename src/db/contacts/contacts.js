d65b577790db252102af3e5c5782550336eeb641
Yura Shaplavsky <yurikorock777@gmail.com>
2025-06-27 23:09:41 +0300

    step4 , add contacts.js

---
+++ src/db/contacts/contacts.js
@@ -0,0 +1,32 @@
+// src/db/contacts/contacts.js
+import { Schema } from 'mongoose';
+
+const contactsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+    },
+    isFavourite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: 'personal',
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
