0a0c599afbcf28348845ee204513113452b48c78
Yura Shaplavsky <yurikorock777@gmail.com>
2025-06-27 22:24:15 +0300

    Mongo connection successfully established!

---
+++ src/db/initMongoConnection.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+import { getEnvVar } from '../utils/getEnvVar.js';
+
+export const initMongoDB = async () => {
+  try {
+    const user = getEnvVar('MONGODB_USER');
+    const pwd = getEnvVar('MONGODB_PASSWORD');
+    const url = getEnvVar('MONGODB_URL');
+    const db = getEnvVar('MONGODB_DB');
+
+    await mongoose.connect(
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
+    );
+    console.log('Mongo connection successfully established!');
+  } catch (e) {
+    console.log('Error while setting up mongo connection', e);
+    throw e;
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 // src/index.js
 
+import { initMongoDB } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
 
-setupServer();
+const bootstrap = async () => {
+  await initMongoDB();
+  setupServer();
+};
+//bootstrap, буде ініціалізувати підключення до бази даних, після чого запускати сервер.
+bootstrap();
