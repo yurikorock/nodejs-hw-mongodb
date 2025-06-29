97aede107969254319b684e3f9edff28620ceab5
Yura Shaplavsky <yurikorock777@gmail.com>
2025-06-29 13:17:26 +0300

    replace app.get contacts in server.js

--- src/server.js
+++ src/server.js
@@ -33,23 +33,6 @@ export const setupServer = () => {
     });
   });
 
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
-
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
-
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-
   //маршрут для отримання колекції всіх контактів
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
@@ -72,5 +55,22 @@ export const setupServer = () => {
       message: 'Successfully found contacts!',
       data: contact,
     });
+  });
+
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
   });
 };
