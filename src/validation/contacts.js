ee661876cd8dc10cd33573a4191709634c2bd76e
Yura Shaplavsky <yurikorock777@gmail.com>
2025-07-11 20:37:54 +0300

    fix bug

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -19,17 +19,12 @@ export const createContactsSchema = Joi.object({
 
 //валідація обєкта контакта при його оновленні
 export const updateContactsSchema = Joi.object({
-  name: Joi.string().min(3).max(20).required(),
-  phoneNumber: Joi.string().min(3).max(20).required(),
+  name: Joi.string().min(3).max(20),
+  phoneNumber: Joi.string().min(3).max(20),
   email: Joi.string()
     .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
     .min(3)
-    .max(20)
-    .optional(),
-  isFavourite: Joi.boolean().optional(),
-  contactType: Joi.string()
-    .min(3)
-    .max(20)
-    .valid('work', 'home', 'personal')
-    .required(),
+    .max(20),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().min(3).max(20).valid('work', 'home', 'personal'),
 });
