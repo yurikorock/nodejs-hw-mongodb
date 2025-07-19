5b314e4d45302c1066cfad5d5ac9eb36ebd2789d
yurii macbook pro <yurikorock777@gmail.com>
2025-07-19 11:50:58 +0300

    end task 4

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,6 @@
 //src/controllers/auth.js
 
+import { ONE_DAY } from '../constants/index,js';
 import { loginUser, registerUsers } from '../services/auth.js';
 
 export const registerUserController = async (req, res) => {
@@ -12,6 +13,26 @@ export const registerUserController = async (req, res) => {
     data: user,
   });
 };
+
+// функція loginUserController обробляє HTTP-запит на вхід користувача,
+// викликає функцію аутентифікації loginUser, встановлює куки для
+// збереження токенів та ідентифікатора сесії, і відправляє клієнту відповідь
+// з інформацією про успішний вхід та токеном доступу.
 export const loginUserController = async (req, res) => {
-  await loginUser(req.body);
+  const session = await loginUser(req.body);
+  res.cookie('refreshToken', session.refreshToken, {
+    httpOnly: true,
+    expires: new Date(Date.now + ONE_DAY),
+  });
+  res.cookie('sessionId', session._id, {
+    httpOnly: true,
+    expires: new Date((Date.now = ONE_DAY)),
+  });
+  res.json({
+    status: 200,
+    message: 'Successfully logged in an user!',
+    data: {
+      accessToken: session.accessToken,
+    },
+  });
 };
