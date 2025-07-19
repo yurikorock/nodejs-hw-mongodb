a83d7ce56a23f6ad5f08da27c4a956854e77dc4e
yurii macbook pro <yurikorock777@gmail.com>
2025-07-19 08:02:01 +0300

    fix bug

--- src/routers/index.js
+++ src/routers/index.js
@@ -4,7 +4,9 @@ import { Router } from 'express';
 import contactsRouter from './contacts.js';
 import authRouter from './auth.js';
 
-const router = Router;
+const router = Router();
 
 router.use('/contacts', contactsRouter);
 router.use('/auth', authRouter);
+
+export default router;
