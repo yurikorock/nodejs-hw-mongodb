e2cd912ad9be26afd2a060d3a34317d184154649
yurii macbook pro <yurikorock777@gmail.com>
2025-07-19 09:39:16 +0300

    end create register user

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,5 +1,13 @@
+import bcrypt from 'bcrypt';
+
 import { UsersCollection } from '../db/contacts/user.js';
+import createHttpError from 'http-errors';
 
 export const registerUsers = async (payload) => {
-  return await UsersCollection.create(payload);
+  const user = await UsersCollection.findOne({ email: payload.email });
+  if (user) {
+    throw createHttpError(409, 'Email in use');
+  }
+  const encryptedPassword = await bcrypt.hash(payload.password, 10);
+  return await UsersCollection.create({ ...payload, encryptedPassword });
 };
