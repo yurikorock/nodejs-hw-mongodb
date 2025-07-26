1696a02f48db35aafcadfd9afb808accec71e06b
yurii macbook pro <yurikorock777@gmail.com>
2025-07-26 12:25:13 +0300

    fix bug 2

--- src/routers/index.js
+++ src/routers/index.js
@@ -6,7 +6,7 @@ import authRouter from './auth.js';
 
 const router = Router();
 
-router.use('/contacts', contactsRouter);
 router.use('/auth', authRouter);
+router.use('/contacts', contactsRouter);
 
 export default router;
